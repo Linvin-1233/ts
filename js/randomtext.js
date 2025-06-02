e619e166d6869faef209dd86f8e21bc5ff5a1b51
Linvin_ <weizhihan3@gmail.com>
2025-06-02 16:56:25 +0200

    Update randomtext.js

--- js/randomtext.js
+++ js/randomtext.js
@@ -2,7 +2,7 @@ const texts = [ //文本//
     "Lazy is keep in life",
     "Why so serious?",
     "Ahh, just a very simple bug. I'll fix it in this century",
-    "Why they have girlfriend?",
+    "Why all they have girlfriend?",
     "F*ck",
     "Why this shit can't running? I didn't find any problem of this shit",
     "I don't car",
